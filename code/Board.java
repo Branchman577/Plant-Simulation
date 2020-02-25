823f38ac3448b10f2552cf216ed4e9efdd7f682a
omancrn2 <nevan.omancrowe2@mail.dcu.ie>
2020-02-25 17:09:29 +0000

    Changed how resrources are placed on the board adding min of one each and seperating out rocks

--- code/Board.java
+++ code/Board.java
@@ -5,26 +5,22 @@ public class Board{
 	public Integer sizex;
 	public Integer sizey;
 	public Integer noPlants;
-	public Integer noWater;
-	public Integer noIron;
-	public Integer noNitro;
 	public SimProcess[][] boardboi;
+	public Integer resources;
 
 	public Board( Integer sizey, Integer sizex, Integer noPlants, LaunchSimulation name){
 		this.sizex = sizex;
 		this.sizey = sizey;
 		this.noPlants = noPlants;
-		int resources = ((sizex * sizey)/16);
-		this.noWater = resources;
-		this.noIron = resources;
-		this.noNitro = resources;
+		this.resources = ((this.sizex * this.sizey)/16);
 		this.boardboi = makeboard(name);
 	}
 
 	public SimProcess[][] makeboard(LaunchSimulation name){
 		double plantcount = (double)this.noPlants;
 		double plantprob;
-		double resourcecount = (plantcount + 2) * 2; // plus 2 for stones
+		double resourcecount = this.resources;
+		double numrocks = this.resources/4;
 		double resourceprob;
 
 		SimProcess[][] returnarray = new SimProcess[this.sizey +2 ][this.sizex]; // plus 2 for sky
@@ -51,6 +47,9 @@ public class Board{
 			}
 		}
 		//Placing resources
+		int minwater =1;
+		int miniron = 1;
+		int minnitro= 1;
 		for (int l =4; l < returnarray.length; l++ ) {
 			for (int k =0; k < returnarray[l].length; k++ ) {
 				resourceprob = probplacer((double)this.sizey,(double)this.sizex,resourcecount,0.3,k,l);
@@ -58,19 +57,49 @@ public class Board{
 				if (returnarray[l][k]== null) {
 					if ((resourceprob >=1 || Math.random()< resourceprob) && resourcecount != 0) {
 
-						int numtype = (int)(Math.random() * ((5 - 2) + 1)) + 2; // random number between 2 and 5 inclusive
-						if(numtype == 5)
-							returnarray[l][k] = new Resource(5,-1.0,name,"Rock",true,l,k);
-						else
-							returnarray[l][k] = new Resource(numtype,1000.0,name,"Resource",true,l,k);
-//						returnarray[l][k].activate();
+						int numtype = (int)(Math.random() * ((4 - 2) + 1)) + 2; // random number between 2 and 4 inclusive
+						if(minwater == 1){
+							returnarray[l][k] = new Resource(2,1000.0,name,"Resource",true,l,k);//min 1 water tile
+							minwater =0;
+						}
+						else if(miniron == 1){
+							returnarray[l][k] = new Resource(3,1000.0,name,"Resource",true,l,k);//min 1 iron tile
+							miniron =0;
+						}
+						else if (minnitro == 1){
+							returnarray[l][k] = new Resource(4,1000.0,name,"Resource",true,l,k);//min 1 iron tile
+							minnitro =0;
+						}
+						else if ((minwater ==0 && miniron == 0) && minnitro== 0) {
+							returnarray[l][k] = new Resource(numtype,1000.0,name,"Resource",true,l,k);//random choice the rest of resources
+						}
+							
 						resourcecount -=1;
+					}
+				} 
+			}
+		}
+		// placing rocks
+		for (int l =4; l < returnarray.length; l++ ) {
+			for (int k =0; k < returnarray[l].length; k++ ) {
+				resourceprob = probplacer((double)this.sizey,(double)this.sizex,numrocks,0.3,k,l);
+
+				if (returnarray[l][k]== null) {
+					if ((resourceprob >=1 || Math.random()< resourceprob) && numrocks != 0) {
+						returnarray[l][k] = new Resource(5,1000.0,name,"Resource",true,l,k);
+						numrocks -=1;
 					}
 				}
 			}
 		}
+
 		return returnarray;
 	}
+	// Nedd to ad rocks
+		//if(numtype == 5 && numrocks !=0){
+							//returnarray[l][k] = new Resource(5,-1.0,name,"Rock",true,l,k);
+							//numrocks -=1;
+						//}
 	public double probplacer(double rows, double columns,double numtoplace, double base, int scanx,int scany){
 		//System.out.println(rows+" "+columns+" "+numtoplace+" "+base+" "+scanx+" "+scany);
 		double num_of_tiles = (columns - scanx) + (columns *((rows-1.0)-(scany-2)));
